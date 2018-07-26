5d14f887b4c11b01089f8717308861886a1b96c9
Lubomír Ježek <lj@mcrai.eu>
2018-07-26 11:12:08 +0200

    fade in on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
+import { fadeInAnimation } from './utility/animation/fade-in';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    fadeInAnimation
+  ]
 })
 export class AppComponent { }
---
+++ src/app/utility/animation/fade-in.ts
@@ -0,0 +1,9 @@
+import { trigger, style, animate, transition, state } from '@angular/animations';
+
+export const fadeInAnimation = trigger('fadeIn', [
+  state('in', style({ height: '*' })),
+  transition(':enter', [
+    style({ opacity: 0 }),
+    animate('2000ms cubic-bezier(.25, .8, .25, 1)', style({ opacity: 1 }))
+  ])
+]);
